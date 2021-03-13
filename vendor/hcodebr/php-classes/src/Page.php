87f92b8ee1bee842d139275aa5021208c04154e9
Matheus henrique <matheus.mgbh1@gmail.com>
2021-03-13 20:13:18 -0300

    admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -11,12 +11,12 @@ class Page
     [
         "data"=>[]
     ];
-    public function __construct($opts = array())
+    public function __construct($opts = array(), $tpl_dir = "/views/")
     {
         $this->options = array_merge($this->dafaults,$opts);
 
         $config = array(
-            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
             "debug"         => true // set to false to improve the speed
         );
